f16e3e52bf68ca1e0cbdb63568778127ee15016f
jfarcand <jfarcand@apache.org>
2012-02-23 10:08:24 -0500

    trefactor

---
+++ src/main/java/org/atmosphere/nettosphere/ChannelAsyncIOWriter.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2012 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.nettosphere;
+
+import org.atmosphere.cpr.AsyncIOWriter;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBufferOutputStream;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
+import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+import org.jboss.netty.handler.codec.http.HttpVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * An {@link AsyncIOWriter} that bridge Atmosphere output stream with Netty's Channel.
+ */
+public class ChannelAsyncIOWriter implements AsyncIOWriter {
+    private static final Logger logger = LoggerFactory.getLogger(ChannelAsyncIOWriter.class);
+
+    private final Channel channel;
+    private final AtomicInteger pendingWrite = new AtomicInteger();
+    private final AtomicBoolean asyncClose = new AtomicBoolean(false);
+    private final ML listener = new ML();
+    private boolean resumeOnBroadcast = false;
+    private boolean byteWritten = false;
+    private final AtomicBoolean isClosed = new AtomicBoolean(false);
+    private boolean headerWritten = false;
+    private final static String END = Integer.toHexString(0);
+    private final static byte[] CHUNK_DELIMITER = "\r\n".getBytes();
+    private final static byte[] ENDCHUNK = (END + "\r\n\r\n").getBytes();
+
+    public ChannelAsyncIOWriter(Channel channel) {
+        this.channel = channel;
+    }
+
+    public boolean isClosed() {
+        return isClosed.get();
+    }
+
+    public boolean byteWritten() {
+        return byteWritten;
+    }
+
+    public void resumeOnBroadcast(boolean resumeOnBroadcast) {
+        this.resumeOnBroadcast = resumeOnBroadcast;
+    }
+
+    @Override
+    public void redirect(String location) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void writeError(int errorCode, String message) throws IOException {
+        if (!channel.isOpen()) {
+            return;
+        }
+
+        try {
+            DefaultHttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1,
+                    HttpResponseStatus.valueOf(errorCode));
+            channel.write(response).addListener(ChannelFutureListener.CLOSE);
+        } catch (Throwable ex) {
+            logger.debug("", ex);
+        }
+    }
+
+    @Override
+    public void write(String data) throws IOException {
+        byte[] b = data.getBytes("ISO-8859-1");
+        write(b);
+    }
+
+    @Override
+    public void write(byte[] data) throws IOException {
+        write(data, 0, data.length);
+    }
+
+    @Override
+    public void write(byte[] data, int offset, int length) throws IOException {
+
+        if (channel.isOpen()) {
+            pendingWrite.incrementAndGet();
+            final ChannelBuffer buffer = ChannelBuffers.dynamicBuffer();
+
+            ChannelBufferOutputStream c = new ChannelBufferOutputStream(buffer);
+
+            if (headerWritten) {
+                c.write(Integer.toHexString(length - offset).getBytes("UTF-8"));
+                c.write(CHUNK_DELIMITER);
+            }
+
+            c.write(data, offset, length);
+            if (headerWritten) {
+                c.write(CHUNK_DELIMITER);
+            }
+
+            channel.write(c.buffer()).addListener(listener);
+            byteWritten = true;
+        } else {
+            logger.warn("Channel closed {}", channel);
+        }
+        headerWritten = true;
+    }
+
+    @Override
+    public void flush() {
+    }
+
+    @Override
+    public void close() throws IOException {
+        asyncClose.set(true);
+        if (pendingWrite.get() == 0 && channel.isOpen()) {
+            _close();
+        }
+    }
+
+    private final class ML implements ChannelFutureListener {
+        @Override
+        public void operationComplete(ChannelFuture future) throws Exception {
+            if (channel.isOpen() && (!future.isSuccess() || (pendingWrite.decrementAndGet() == 0 && (resumeOnBroadcast || asyncClose.get())))) {
+                _close();
+            }
+        }
+    }
+
+    void _close() {
+        if (!isClosed.getAndSet(true)) {
+            headerWritten = false;
+            final ChannelBuffer buffer = ChannelBuffers.dynamicBuffer();
+            ChannelBufferOutputStream c = new ChannelBufferOutputStream(buffer);
+            try {
+                c.write(ENDCHUNK);
+                channel.write(buffer).addListener(ChannelFutureListener.CLOSE);
+            } catch (IOException e) {
+                logger.trace("Close error", e);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/atmosphere/nettosphere/NettyWebSocket.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2012 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.nettosphere;
+
+import org.atmosphere.cpr.ApplicationConfig;
+import org.atmosphere.cpr.AtmosphereConfig;
+import org.atmosphere.websocket.WebSocketAdapter;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBufferFactory;
+import org.jboss.netty.buffer.HeapChannelBufferFactory;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import org.jboss.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class NettyWebSocket extends WebSocketAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(NettyWebSocket.class);
+    private final Channel channel;
+    private final AtmosphereConfig config;
+    private final ChannelBufferFactory factory = new HeapChannelBufferFactory();
+
+    public NettyWebSocket(Channel channel, AtmosphereConfig config) {
+        this.channel = channel;
+        this.config = config;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void redirect(String location) throws IOException {
+        logger.error("redirect not supported");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void writeError(int errorCode, String message) throws IOException {
+        logger.debug("{} {}", errorCode, message);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void write(String data) throws IOException {
+        channel.write(new TextWebSocketFrame(data));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void write(byte[] data) throws IOException {
+        String s = config.getInitParameter(ApplicationConfig.WEBSOCKET_BLOB);
+        if (s != null && Boolean.parseBoolean(s)) {
+            ChannelBuffer c = factory.getBuffer(data.length);
+            c.writeBytes(data);
+            channel.write(new BinaryWebSocketFrame(c));
+        } else {
+            channel.write(new TextWebSocketFrame(new String(data, 0, data.length, "UTF-8")));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void write(byte[] data, int offset, int length) throws IOException {
+                String s = config.getInitParameter(ApplicationConfig.WEBSOCKET_BLOB);
+        if (s != null && Boolean.parseBoolean(s)) {
+            ChannelBuffer c = factory.getBuffer(length - offset);
+            c.writeBytes(data);
+            channel.write(new BinaryWebSocketFrame(c));
+        } else {
+            channel.write(new TextWebSocketFrame(new String(data, offset, length, "UTF-8")));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void close() throws IOException {
+        channel.close().addListener(ChannelFutureListener.CLOSE);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void flush() throws IOException {
+    }
+}
