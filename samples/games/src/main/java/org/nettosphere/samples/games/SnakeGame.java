08f97bdcbb765577b1b5f6e76131118df0ad1fc1
jfarcand <jfarcand@apache.org>
2013-03-20 17:36:35 -0400

    Make it work with Server-Side Events

---
+++ samples/games/src/main/java/org/nettosphere/samples/games/SnakeGame.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2012 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.nettosphere.samples.games;
+
+import org.atmosphere.cpr.AtmosphereResource;
+import org.atmosphere.cpr.BroadcasterFactory;
+import org.atmosphere.websocket.WebSocket;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SnakeGame {
+    public static final int PLAYFIELD_WIDTH = 640;
+    public static final int PLAYFIELD_HEIGHT = 480;
+    public static final int GRID_SIZE = 10;
+
+    protected static final AtomicInteger snakeIds = new AtomicInteger(0);
+    protected static final Random random = new Random();
+    protected final SnakeBroadcaster snakeBroadcaster;
+
+    public SnakeGame() {
+        snakeBroadcaster = new SnakeBroadcaster(BroadcasterFactory.getDefault().lookup("/snake", true));
+    }
+
+    public static String getRandomHexColor() {
+        float hue = random.nextFloat();
+        // sat between 0.1 and 0.3
+        float saturation = (random.nextInt(2000) + 1000) / 10000f;
+        float luminance = 0.9f;
+        Color color = Color.getHSBColor(hue, saturation, luminance);
+        return '#' + Integer.toHexString(
+                (color.getRGB() & 0xffffff) | 0x1000000).substring(1);
+    }
+
+    public static Location getRandomLocation() {
+        int x = roundByGridSize(random.nextInt(PLAYFIELD_WIDTH));
+        int y = roundByGridSize(random.nextInt(PLAYFIELD_HEIGHT));
+        return new Location(x, y);
+    }
+
+    private static int roundByGridSize(int value) {
+        value = value + (GRID_SIZE / 2);
+        value = value / GRID_SIZE;
+        value = value * GRID_SIZE;
+        return value;
+    }
+
+    public void onOpen(AtmosphereResource resource) throws IOException {
+        int id = snakeIds.getAndIncrement();
+        resource.getRequest().setAttribute("id", id);
+        Snake snake = new Snake(id, resource);
+
+        resource.getRequest().setAttribute("snake", snake);
+        snakeBroadcaster.addSnake(snake);
+        StringBuilder sb = new StringBuilder();
+        for (Iterator<Snake> iterator = snakeBroadcaster.getSnakes().iterator();
+             iterator.hasNext(); ) {
+            snake = iterator.next();
+            sb.append(String.format("{id: %d, color: '%s'}",
+                    Integer.valueOf(snake.getId()), snake.getHexColor()));
+            if (iterator.hasNext()) {
+                sb.append(',');
+            }
+        }
+        snakeBroadcaster.broadcast(String.format("{'type': 'join','data':[%s]}",
+                sb.toString()));
+    }
+
+    public void onClose(AtmosphereResource resource) {
+        snakeBroadcaster.removeSnake(snake(resource));
+        snakeBroadcaster.broadcast(String.format("{'type': 'leave', 'id': %d}",
+                ((Integer) resource.getRequest().getAttribute("id"))));
+    }
+
+    protected Snake snake(AtmosphereResource resource) {
+        return (Snake) resource.getRequest().getAttribute("snake");
+    }
+
+    protected void onMessage(AtmosphereResource resource, String message) {
+        Snake snake = snake(resource);
+        if ("west".equals(message)) {
+            snake.setDirection(Direction.WEST);
+        } else if ("north".equals(message)) {
+            snake.setDirection(Direction.NORTH);
+        } else if ("east".equals(message)) {
+            snake.setDirection(Direction.EAST);
+        } else if ("south".equals(message)) {
+            snake.setDirection(Direction.SOUTH);
+        }
+    }
+}
---
+++ samples/games/src/main/java/org/nettosphere/samples/games/SnakeManagedService.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2012 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.nettosphere.samples.games;
+
+import org.atmosphere.config.service.Get;
+import org.atmosphere.config.service.ManagedService;
+import org.atmosphere.config.service.Post;
+import org.atmosphere.cpr.AtmosphereResource;
+import org.atmosphere.cpr.AtmosphereResourceEvent;
+import org.atmosphere.cpr.AtmosphereResourceEventListenerAdapter;
+import org.atmosphere.cpr.AtmosphereResourceFactory;
+
+import java.io.IOException;
+
+@ManagedService(path = "/snake")
+public class SnakeManagedService extends SnakeGame {
+
+    @Get
+    public void onOpen(final AtmosphereResource resource) {
+        resource.addEventListener(new AtmosphereResourceEventListenerAdapter() {
+            @Override
+            public void onSuspend(AtmosphereResourceEvent event) {
+                try {
+                    SnakeManagedService.super.onOpen(resource);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onDisconnect(AtmosphereResourceEvent event) {
+                SnakeManagedService.super.onClose(resource);
+            }
+        });
+    }
+
+    @Post
+    public void onMessage(AtmosphereResource resource) {
+        try {
+            // Here we need to find the suspended AtmosphereResource
+            SnakeManagedService.super.onMessage(AtmosphereResourceFactory.getDefault().find(resource.uuid()), resource.getRequest().getReader().readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
