4229619b686eb5065499f9063287c694640d5f93
jfarcand <jfarcand@apache.org>
2013-06-12 15:34:04 -0400

    Add support for Cache Validation

--- server/src/main/java/org/atmosphere/nettosphere/HttpStaticFileServerHandler.java
+++ server/src/main/java/org/atmosphere/nettosphere/HttpStaticFileServerHandler.java
@@ -56,10 +56,18 @@ import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
 import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.DATE;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.IF_MODIFIED_SINCE;
 import static org.jboss.netty.handler.codec.http.HttpHeaders.isKeepAlive;
 import static org.jboss.netty.handler.codec.http.HttpHeaders.setContentLength;
 import static org.jboss.netty.handler.codec.http.HttpMethod.GET;
@@ -67,6 +75,7 @@ import static org.jboss.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
 import static org.jboss.netty.handler.codec.http.HttpResponseStatus.INTERNAL_SERVER_ERROR;
 import static org.jboss.netty.handler.codec.http.HttpResponseStatus.METHOD_NOT_ALLOWED;
 import static org.jboss.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
+import static org.jboss.netty.handler.codec.http.HttpResponseStatus.NOT_MODIFIED;
 import static org.jboss.netty.handler.codec.http.HttpResponseStatus.OK;
 import static org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
@@ -75,22 +84,22 @@ import static org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1;
  * HTTP responses.  It also implements {@code 'If-Modified-Since'} header to
  * take advantage of browser cache, as described in
  * <a href="http://tools.ietf.org/html/rfc2616#section-14.25">RFC 2616</a>.
- *
+ * <p/>
  * <h3>How Browser Caching Works</h3>
- *
+ * <p/>
  * Web browser caching works with HTTP headers as illustrated by the following
  * sample:
  * <ol>
  * <li>Request #1 returns the content of <code>/file1.txt</code>.</li>
  * <li>Contents of <code>/file1.txt</code> is cached by the browser.</li>
  * <li>Request #2 for <code>/file1.txt</code> does return the contents of the
- *     file again. Rather, a 304 Not Modified is returned. This tells the
- *     browser to use the contents stored in its cache.</li>
+ * file again. Rather, a 304 Not Modified is returned. This tells the
+ * browser to use the contents stored in its cache.</li>
  * <li>The server knows the file has not been modified because the
- *     <code>If-Modified-Since</code> date is the same as the file's last
- *     modified date.</li>
+ * <code>If-Modified-Since</code> date is the same as the file's last
+ * modified date.</li>
  * </ol>
- *
+ * <p/>
  * <pre>
  * Request #1 Headers
  * ===================
@@ -117,7 +126,12 @@ import static org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1;
  * </pre>
  */
 public class HttpStaticFileServerHandler extends SimpleChannelUpstreamHandler {
+    public static final String HTTP_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss zzz";
+    public static final String HTTP_DATE_GMT_TIMEZONE = "GMT";
+    public static final int HTTP_CACHE_SECONDS = 60;
+
     private final static Logger logger = LoggerFactory.getLogger(HttpStaticFileServerHandler.class);
+
     public final static String STATIC_MAPPING = SimpleChannelUpstreamHandler.class.getName() + ".staticMapping";
     public final static String SERVICED = SimpleChannelUpstreamHandler.class.getName() + ".serviced";
     private final List<String> paths;
@@ -136,7 +150,8 @@ public class HttpStaticFileServerHandler extends SimpleChannelUpstreamHandler {
 
         RandomAccessFile raf = null;
         boolean found = true;
-        for (String p: paths) {
+        File file = null;
+        for (String p : paths) {
             String path = p + sanitizeUri(request.getUri());
             if (path.endsWith("/") || path.endsWith(File.separator)) {
                 path += "index.html";
@@ -147,7 +162,7 @@ public class HttpStaticFileServerHandler extends SimpleChannelUpstreamHandler {
                 continue;
             }
 
-            File file = new File(path);
+            file = new File(path);
             if (file.isHidden() || !file.exists()) {
                 found = false;
                 continue;
@@ -172,6 +187,22 @@ public class HttpStaticFileServerHandler extends SimpleChannelUpstreamHandler {
             return;
         }
 
+        // Cache Validation
+        String ifModifiedSince = request.getHeader(IF_MODIFIED_SINCE);
+        if (file != null && ifModifiedSince != null && ifModifiedSince.length() != 0) {
+            SimpleDateFormat dateFormatter = new SimpleDateFormat(HTTP_DATE_FORMAT, Locale.US);
+            Date ifModifiedSinceDate = dateFormatter.parse(ifModifiedSince);
+
+            // Only compare up to the second because the datetime format we send to the client does
+            // not have milliseconds
+            long ifModifiedSinceDateSeconds = ifModifiedSinceDate.getTime() / 1000;
+            long fileLastModifiedSeconds = file.lastModified() / 1000;
+            if (ifModifiedSinceDateSeconds == fileLastModifiedSeconds) {
+                sendNotModified(ctx);
+                return;
+            }
+        }
+
         long fileLength = raf.length();
 
         HttpResponse response = new DefaultHttpResponse(HTTP_1_1, OK);
@@ -190,7 +221,7 @@ public class HttpStaticFileServerHandler extends SimpleChannelUpstreamHandler {
         } else {
             // No encryption - use zero-copy.
             final FileRegion region =
-                new DefaultFileRegion(raf.getChannel(), 0, fileLength);
+                    new DefaultFileRegion(raf.getChannel(), 0, fileLength);
             writeFuture = ch.write(region);
             writeFuture.addListener(new ChannelFutureProgressListener() {
                 public void operationComplete(ChannelFuture future) {
@@ -255,6 +286,22 @@ public class HttpStaticFileServerHandler extends SimpleChannelUpstreamHandler {
         }
 
         return uri;
+    }
+
+    private static void sendNotModified(ChannelHandlerContext ctx) {
+        HttpResponse response = new DefaultHttpResponse(HTTP_1_1, NOT_MODIFIED);
+        setDateHeader(response);
+
+        // Close the connection as soon as the error message is sent.
+        ctx.getChannel().write(response).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    private static void setDateHeader(HttpResponse response) {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat(HTTP_DATE_FORMAT, Locale.US);
+        dateFormatter.setTimeZone(TimeZone.getTimeZone(HTTP_DATE_GMT_TIMEZONE));
+
+        Calendar time = new GregorianCalendar();
+        response.setHeader(DATE, dateFormatter.format(time.getTime()));
     }
 
     protected void sendError(ChannelHandlerContext ctx, HttpResponseStatus status, MessageEvent e) {
