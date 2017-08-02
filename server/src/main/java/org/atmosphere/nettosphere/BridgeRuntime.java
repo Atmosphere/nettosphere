94f6074aec577c44fab5f3b61b4a2f2350b26b0b
Seamus McMorrow <smcmorrow.ardan1@gmail.com>
2017-08-02 13:09:38 +0100

    Fix handleWebSocketFrame copying the content to AtmosphereResource.

--- server/src/main/java/org/atmosphere/nettosphere/BridgeRuntime.java
+++ server/src/main/java/org/atmosphere/nettosphere/BridgeRuntime.java
@@ -415,7 +415,7 @@ public class BridgeRuntime extends HttpStaticFileServerHandler {
         byte[] body = null;
         if (binaryData.isReadable()) {
             body = new byte[binaryData.readableBytes()];
-            binaryData.readBytes(body);
+            binaryData.copy().readBytes(body);
         }
         if (frame instanceof CloseWebSocketFrame) {
             ctx.channel().write(frame).addListener(ChannelFutureListener.CLOSE);
