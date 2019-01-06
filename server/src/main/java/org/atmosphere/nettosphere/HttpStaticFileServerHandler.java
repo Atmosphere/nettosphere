3774aa166ae7f689f8d67d33904a954c9142060f
jfarcand <jfarcand@apache.org>
2019-01-06 16:27:03 -0500

    Update Copyright

--- server/src/main/java/org/atmosphere/nettosphere/HttpStaticFileServerHandler.java
+++ server/src/main/java/org/atmosphere/nettosphere/HttpStaticFileServerHandler.java
@@ -14,7 +14,7 @@
  * under the License.
  */
 /*
- * Copyright 2008-2018 Async-IO.org
+ * Copyright 2008-2019 Async-IO.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
